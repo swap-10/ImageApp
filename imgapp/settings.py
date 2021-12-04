030ca630de134db5edac593868ff646bda41174b
swap-10 <swapnalv10@gmail.com>
2021-12-04 21:02:30 +0530

    Fix staticfiles error in production

--- imgapp/settings.py
+++ imgapp/settings.py
@@ -137,7 +137,7 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 
-
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 
 STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
