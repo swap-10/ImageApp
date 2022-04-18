eeb5ae54fdfe8c2ecd705b5862e12acfc29506bb
swap-10 <swapnalv10@gmail.com>
2022-04-18 17:49:48 +0530

    Modify allowed hosts to new url

--- imgapp/settings.py
+++ imgapp/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
-ALLOWED_HOSTS = ['img-db-app.herokuapp.com']
+ALLOWED_HOSTS = ['pixsea.herokuapp.com']
 
 
 # Application definition
